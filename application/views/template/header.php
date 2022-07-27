4e04fc9cada077fcdff1c6859c8492b704934037
Muhamad Zaidan Yahya <zaidan.nganjuk1122@gmail.com>
2022-07-27 17:10:28 +0700

    generate pdf

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -30,7 +30,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?php echo site_url('Main/index') ?>">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?php echo site_url('C_dashboard/index') ?>">
 
 
 
